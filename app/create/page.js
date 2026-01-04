14fab5ccfd9929f1c0cfd3eae84ebdd5b7cc1539
solenx-ux <xyzrowdy08@gmail.com>
2026-01-04 10:21:44 +0530

    Create page.js

---
+++ app/create/page.js
@@ -0,0 +1,8 @@
+export default function CreatePage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Create Video Page</h1>
+      <p>If you see this, routing works.</p>
+    </div>
+  );
+}
